4935296ad032aea1e78c4e45b3d765dad67540fa
Nikunj <mnikunj262@gmail.com>
2025-04-17 13:17:27 +0530

    test: path practice to generate diff from single seed

--- src/index.ts
+++ src/index.ts
@@ -8,24 +8,37 @@ console.log("Generated Mnemonic:", mnemonic);
 
 const seed = mnemonicToSeedSync(mnemonic);
 
-
-const path = `m/44'/501'/0'/0'`
 const hexSeed = seed.toString('hex');
+
+// path 1
+const path = `m/44'/501'/0'/0'`
 const deriveSeed = derivePath(path, hexSeed).key;
-
 const secret = nacl.sign.keyPair.fromSeed(deriveSeed).secretKey;
-
 const publicKey = Keypair.fromSecretKey(secret).publicKey;
+
+// path 2
+const path2 = `m/44'/501'/1'/0'`
+const deriveSeed2 = derivePath(path2, hexSeed).key;
+const secret2 = nacl.sign.keyPair.fromSeed(deriveSeed2).secretKey;
+const publicKey2 = Keypair.fromSecretKey(secret2).publicKey;
 
 const message = new TextEncoder().encode("hello world");
 
 const signature = nacl.sign.detached(message, secret);
+const signature2 = nacl.sign.detached(message, secret2);
 
 // all value are in Uint8
 const result = nacl.sign.detached.verify(
     message,
-    signature,
+    signature2,
     publicKey.toBytes()
-)
+) // false becuse it sign by secret2 and here verify by public 1
 
+const result2 = nacl.sign.detached.verify(
+    message,
+    signature2,
+    publicKey2.toBytes()
+) // true sign by secret2 abd verfiy by public 2
 console.log(result);
+
+console.log(result2)
